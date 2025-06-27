c7015959da51d51bda48c03fda2608352a52e0f5
Yogesh Saini <yogeshsaini7172@gmail.com>
2025-06-28 01:32:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// let handH=document.getElementsByClassName("hand1");
+// let handM=document.getElementsByClassName("hand2");
+// let handS=document.getElementsByName("hand3");
+// handS.style.transform="rotate(45deg)";
+// setInterval(
+//     function(){
+//         let date=new date();
+//         let se=date.getSeconds()*6;
+//         // let min=date.getMinutes()*6;
+//         // let hr=date.getMinutes()*0.5;
+//         handS.style.transform="rotate("+se+"deg)";
+//         // handM.style.transform="rotate("+min+"deg)";
+//         // handH.style.transform="rotate("+hr+"deg)";
+//     },1000
+// );
